5f2851b6f7aa82fe81af0daf578c6af17e6a9ce4
Gozde SAYGILI YALCIN <saygiligozde@gmail.com>
2022-06-21 09:04:28 +0300

    "first commit"

---
+++ src/dev/patika/java101/HarmonicAvarage.java
@@ -0,0 +1,18 @@
+package dev.patika.java101;
+
+public class HarmonicAvarage {
+
+	public static void main(String[] args) {
+		// This is an example that can calculate harmonical avarage for array.
+		
+				int [] list = {1,2,3,4,5,6,7,8,9,10}; 
+				
+				double sumHarm = 0;
+				for(double i=1; i<list.length; i++) {
+					sumHarm=sumHarm+1/i;
+				}
+				double avarage = (list.length)/sumHarm;
+				System.out.println("Harmonic avarage is: " + avarage);
+	}
+
+}
