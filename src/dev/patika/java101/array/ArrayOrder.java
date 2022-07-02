a29fad8602d1d95c63d7abd14e31b881bb66484e
Gozde SAYGILI YALCIN <saygiligozde@gmail.com>
2022-07-02 14:49:12 +0300

    "comment is fixed"

--- src/dev/patika/java101/array/ArrayOrder.java
+++ src/dev/patika/java101/array/ArrayOrder.java
@@ -10,6 +10,7 @@ public class ArrayOrder {
 		Scanner input = new Scanner(System.in);
 		System.out.print("please enter the elements of the array: ");
 	    int number = input.nextInt();
+	    
 	    int[] n1 = new int[number];
 	    for(int i = 0; i < number; i++){
 	        n1[i]=input.nextInt();
@@ -17,13 +18,14 @@ public class ArrayOrder {
 
 	        input.close();
 
-	        // Prints each sequential element in array a
-	        for (int i = 0; i < n1.length; i++) {
-	            System.out.println((i+1) +"." +" element is: " +n1[i]);
-	        }
-	        System.out.println("The sorted int array is:"); 
-	        Arrays.sort(n1);
-	        System.out.print(Arrays.toString(n1));
+		        // Prints each sequential element in array n1
+		        for (int i = 0; i < n1.length; i++) {
+		            System.out.println((i+1) +"." +" element is: " +n1[i]);
+		        }
+		        
+		        System.out.println("The sorted int array is:"); 
+		        Arrays.sort(n1);
+		        System.out.print(Arrays.toString(n1));
     }
 }
 
