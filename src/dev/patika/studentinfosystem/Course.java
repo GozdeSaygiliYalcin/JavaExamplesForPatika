ca6e54db62f26861c90144053ed4027b08501aa3
Gozde SAYGILI YALCIN <saygiligozde@gmail.com>
2022-07-23 15:12:14 +0300

    first commit

---
+++ src/dev/patika/studentinfosystem/Course.java
@@ -0,0 +1,9 @@
+package dev.patika.studentinfosystem;
+
+public class Course {
+
+	//name,stuNo,classes,course1,course2,course3,avarage,isPass
+	
+	private String name;
+	
+}
---
+++ src/dev/patika/studentinfosystem/Teacher.java
@@ -0,0 +1,5 @@
+package dev.patika.studentinfosystem;
+
+public class Teacher {
+
+}
