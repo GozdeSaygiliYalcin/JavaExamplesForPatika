99124295fbf03729c0f6b6678db7779270c53868
Gozde SAYGILI YALCIN <saygiligozde@gmail.com>
2022-07-31 22:40:39 +0300

    "first commit"

--- src/dev/patika/java101/PalindromNumbers.java
+++ src/dev/patika/java101/PalindromNumbers.java
@@ -3,23 +3,22 @@ package dev.patika.java101;
 public class PalindromNumbers {
 	
 	public static boolean isPalindrome(int number) {
-
-    int temp = number;
-    int reverseNum=0,lastNum;
-    
-    while(temp>0){
-        lastNum = temp % 10;
-        reverseNum = reverseNum*10 + lastNum;
-        temp /= 10;
-    }
-    if(number == reverseNum){
-        return true;
-    }
-    else{
-        return false;
-    }
-
-}
+	
+	    int temp = number;
+	    int reverseNum=0,lastNum;
+	    
+	    while(temp>0){
+	        lastNum = temp % 10;
+	        reverseNum = reverseNum*10 + lastNum;
+	        temp /= 10;
+	    }
+	    if(number == reverseNum){
+	        return true;
+	    }
+	    else{
+	        return false;
+	    }
+	}	
 
 	public static void main(String[] args) {
 		// This is an example that provides to find palindrom numbers.
