e70fb461de7053786d46319c996b230e32392ee1
Gozde SAYGILI YALCIN <saygiligozde@gmail.com>
2022-09-01 18:36:47 +0300

    salary calculator is added

---
+++ src/dev/patika/java101/salarycalculator/App.java
@@ -0,0 +1,11 @@
+package dev.patika.java101.salarycalculator;
+
+public class App {
+
+	public static void main(String[] args) {
+	    Employee salary = new Employee("Harry Potter", 1000, 15, 1000);
+        salary.total(salary);
+
+	}
+
+}
